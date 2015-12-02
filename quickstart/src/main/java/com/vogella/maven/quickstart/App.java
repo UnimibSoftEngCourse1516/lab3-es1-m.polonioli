d3ab9fa6b0d4d453a2b2a3159f0c3e2b98f9f52a
Michele Polonioli <m.polonioli@campus.unimib.it>
2015-12-02 09:13:32 +0100

    Add content in class "App.java"

--- quickstart/src/main/java/com/vogella/maven/quickstart/App.java
+++ quickstart/src/main/java/com/vogella/maven/quickstart/App.java
@@ -1,5 +1,6 @@
 package com.vogella.maven.quickstart;
 
+import com.google.gson.Gson;
 /**
  * Hello world!
  *
@@ -8,6 +9,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Gson gson = new Gson();
         System.out.println( "Hello World!" );
     }
 }
